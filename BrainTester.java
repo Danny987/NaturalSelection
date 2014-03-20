cad6fefc0c6305faddbffea31ce91fc8f8bac24d
Danny987 <crazydrums987@gmail.com>
2014-03-20 16:02:14 -0600

    Basic hillclimbing strategy selection

---
+++ BrainTester.java
@@ -0,0 +1,16 @@
+package danny;
+
+public class BrainTester {
+
+	public BrainTester(){
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Brain brain = new Brain();
+	}
+}
