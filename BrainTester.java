7c73ea5b5927c4fa566705c3e972d8cca4557b5e
Danny987 <crazydrums987@gmail.com>
2014-04-03 20:19:59 -0600

    Hill climbing joints - can't add rules or blocks yet

---
+++ BrainTester.java
@@ -0,0 +1,171 @@
+package creature.geeksquad.hillclimbing;
+
+import java.util.ArrayList;
+
+import creature.geeksquad.genetics.Allele;
+import creature.geeksquad.genetics.Gene;
+import creature.geeksquad.genetics.GeneticsException;
+import creature.geeksquad.genetics.Genotype;
+import creature.geeksquad.genetics.Allele.Trait;
+import creature.geeksquad.genetics.Hopper;
+import creature.phenotype.Block;
+import creature.phenotype.EnumJointSite;
+import creature.phenotype.EnumJointType;
+import creature.phenotype.EnumNeuronInputType;
+import creature.phenotype.EnumOperatorBinary;
+import creature.phenotype.EnumOperatorUnary;
+import creature.phenotype.NeuronInput;
+
+public class BrainTester {
+
+	public BrainTester(){
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		ArrayList<Allele> alleles = new ArrayList<Allele>();
+		ArrayList<Gene> genes;
+		// Adding some dummy Alleles to the list.
+		alleles.add(new Allele(Trait.LENGTH, 35.4f, 0.3f));
+		alleles.add(new Allele(Trait.LENGTH, 13.3f, 0.64f));
+		alleles.add(new Allele(Trait.HEIGHT, 42.5f, 0.5f));
+		alleles.add(new Allele(Trait.HEIGHT, 20.5f, 0.35f));
+		alleles.add(new Allele(Trait.WIDTH, 42.5f, 0.5f));
+		alleles.add(new Allele(Trait.WIDTH, 20.5f, 0.35f));
+		alleles.add(new Allele(Trait.INDEX_TO_PARENT, Block.PARENT_INDEX_NONE,
+				0.63f));
+		alleles.add(new Allele(Trait.INDEX_TO_PARENT, 1, 0.4f));
+		alleles.add(new Allele(Trait.JOINT_TYPE, EnumJointType.SPHERICAL,
+				0.2f));
+		alleles.add(new Allele(Trait.JOINT_TYPE, EnumJointType.HINGE, 0.0f));
+		// Second block.
+		alleles.add(new Allele(Trait.LENGTH, 21.4f, 0.2f));
+		alleles.add(new Allele(Trait.LENGTH, 20.0f, 0.199f));
+		alleles.add(new Allele(Trait.HEIGHT, 40.5f, 0.1f));
+		alleles.add(new Allele(Trait.HEIGHT, 45.5f, 0.4f));
+		alleles.add(new Allele(Trait.WIDTH, 19.5f, 0.5f));
+		alleles.add(new Allele(Trait.WIDTH, 25.5f, 0.6f));
+		alleles.add(new Allele(Trait.INDEX_TO_PARENT, 0, 0.63f));
+		alleles.add(new Allele(Trait.INDEX_TO_PARENT, 1, 0.4f));
+		alleles.add(new Allele(Trait.JOINT_TYPE, EnumJointType.TWIST, 0.0f));
+		alleles.add(new Allele(Trait.JOINT_TYPE, EnumJointType.SPHERICAL,
+				0.2f));
+		alleles.add(new Allele(Trait.JOINT_ORIENTATION, 0.5f, 0.5f));
+		alleles.add(new Allele(Trait.JOINT_ORIENTATION, 0.5f, 0.5f));
+		alleles.add(new Allele(Trait.JOINT_SITE_ON_PARENT,
+				EnumJointSite.EDGE_FRONT_NORTH, 0.6f));
+		alleles.add(new Allele(Trait.JOINT_SITE_ON_PARENT,
+				EnumJointSite.EDGE_BACK_EAST, 0.3f));
+		alleles.add(new Allele(Trait.JOINT_SITE_ON_CHILD,
+				EnumJointSite.EDGE_BACK_SOUTH, 0.6f));
+		alleles.add(new Allele(Trait.JOINT_SITE_ON_CHILD,
+				EnumJointSite.EDGE_BACK_WEST, 0.7f));
+		alleles.add(new Allele(Trait.RULE_INPUT_A,
+				new NeuronInput(EnumNeuronInputType.TIME), 0.3f));
+		alleles.add(new Allele(Trait.RULE_INPUT_A,
+				new NeuronInput(EnumNeuronInputType.TIME), 0.25f));
+		alleles.add(new Allele(Trait.RULE_INPUT_B,
+				new NeuronInput(EnumNeuronInputType.TIME), 0.3f));
+		alleles.add(new Allele(Trait.RULE_INPUT_B,
+				new NeuronInput(EnumNeuronInputType.TIME), 0.25f));
+		alleles.add(new Allele(Trait.RULE_INPUT_C,
+				new NeuronInput(EnumNeuronInputType.TIME), 0.3f));
+		alleles.add(new Allele(Trait.RULE_INPUT_C,
+				new NeuronInput(EnumNeuronInputType.TIME), 0.25f));
+		alleles.add(new Allele(Trait.RULE_INPUT_D,
+				new NeuronInput(EnumNeuronInputType.TIME), 0.3f));
+		alleles.add(new Allele(Trait.RULE_INPUT_D,
+				new NeuronInput(EnumNeuronInputType.TIME), 0.25f));
+		alleles.add(new Allele(Trait.RULE_INPUT_E,
+				new NeuronInput(EnumNeuronInputType.TIME), 0.3f));
+		alleles.add(new Allele(Trait.RULE_INPUT_E,
+				new NeuronInput(EnumNeuronInputType.TIME), 0.25f));
+		alleles.add(new Allele(Trait.BINARY_OPERATOR_1,
+				EnumOperatorBinary.ADD, 0.2f));
+		alleles.add(new Allele(Trait.BINARY_OPERATOR_1,
+				EnumOperatorBinary.SUBTRACT, 0.1f));
+		alleles.add(new Allele(Trait.UNARY_OPERATOR_2,
+				EnumOperatorUnary.ABS,0.3f));
+		alleles.add(new Allele(Trait.UNARY_OPERATOR_2,
+				EnumOperatorUnary.EXP,0.2f));
+		alleles.add(new Allele(Trait.BINARY_OPERATOR_3,
+				EnumOperatorBinary.ADD, 0.2f));
+		alleles.add(new Allele(Trait.BINARY_OPERATOR_3,
+				EnumOperatorBinary.SUBTRACT, 0.1f));
+		alleles.add(new Allele(Trait.UNARY_OPERATOR_4,
+				EnumOperatorUnary.ABS,0.3f));
+		alleles.add(new Allele(Trait.UNARY_OPERATOR_4,
+				EnumOperatorUnary.EXP,0.2f));
+		alleles.add(new Allele(Trait.DOF_MARKER, EnumJointType.DOF_2, 0.1f));
+		alleles.add(new Allele(Trait.DOF_MARKER, EnumJointType.DOF_2, 0.1f));
+		alleles.add(new Allele(Trait.RULE_INPUT_A,
+				new NeuronInput(EnumNeuronInputType.TIME), 0.3f));
+		alleles.add(new Allele(Trait.RULE_INPUT_A, new NeuronInput(
+				EnumNeuronInputType.TIME), 0.25f));
+		alleles.add(new Allele(Trait.RULE_INPUT_B, new NeuronInput(
+				EnumNeuronInputType.TIME), 0.3f));
+		alleles.add(new Allele(Trait.RULE_INPUT_B, new NeuronInput(
+				EnumNeuronInputType.TIME), 0.25f));
+		alleles.add(new Allele(Trait.RULE_INPUT_C, new NeuronInput(
+				EnumNeuronInputType.TIME), 0.3f));
+		alleles.add(new Allele(Trait.RULE_INPUT_C, new NeuronInput(
+				EnumNeuronInputType.TIME), 0.25f));
+		alleles.add(new Allele(Trait.RULE_INPUT_D, new NeuronInput(
+				EnumNeuronInputType.TIME), 0.3f));
+		alleles.add(new Allele(Trait.RULE_INPUT_D, new NeuronInput(
+				EnumNeuronInputType.TIME), 0.25f));
+		alleles.add(new Allele(Trait.RULE_INPUT_E, new NeuronInput(
+				EnumNeuronInputType.TIME), 0.3f));
+		alleles.add(new Allele(Trait.RULE_INPUT_E, new NeuronInput(
+				EnumNeuronInputType.TIME), 0.25f));
+		alleles.add(new Allele(Trait.BINARY_OPERATOR_1, EnumOperatorBinary.ADD,
+				0.2f));
+		alleles.add(new Allele(Trait.BINARY_OPERATOR_1,
+				EnumOperatorBinary.SUBTRACT, 0.1f));
+		alleles.add(new Allele(Trait.UNARY_OPERATOR_2, EnumOperatorUnary.ABS,
+				0.3f));
+		alleles.add(new Allele(Trait.UNARY_OPERATOR_2, EnumOperatorUnary.EXP,
+				0.2f));
+		alleles.add(new Allele(Trait.BINARY_OPERATOR_3, EnumOperatorBinary.ADD,
+				0.2f));
+		alleles.add(new Allele(Trait.BINARY_OPERATOR_3,
+				EnumOperatorBinary.SUBTRACT, 0.1f));
+		alleles.add(new Allele(Trait.UNARY_OPERATOR_4, EnumOperatorUnary.ABS,
+				0.3f));
+		alleles.add(new Allele(Trait.UNARY_OPERATOR_4, EnumOperatorUnary.EXP,
+				0.2f));
+
+		genes = Gene.allelesToGenes(alleles);
+
+		
+		//initialize hopper to hill climb
+		Hopper hopper = null;
+		//initialize genotype used to create the hopper
+		Genotype genotype = null;
+		
+		
+		//create genotype and hoper
+		try {
+			genotype = new Genotype(genes);
+			hopper = new Hopper(genotype);
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (GeneticsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//create a new brain for this tribe
+		TribeBrain brain = new TribeBrain();
+		
+		//send hopper to hill climbing
+		//returns a hill climbed hopper
+		hopper = brain.performHillClimbing(hopper);
+		
+	}
+}
---
+++ TribeBrain.java
@@ -0,0 +1,137 @@
+package creature.geeksquad.hillclimbing;
+
+import java.util.HashMap;
+
+import creature.geeksquad.genetics.GeneticsException;
+import creature.geeksquad.genetics.Genotype;
+import creature.geeksquad.genetics.Hopper;
+
+import creature.geeksquad.library.*;
+
+/**
+ * 
+ * @author Daniel
+ * 
+ * The tribe brain object contains a map of hill climbing strategies that
+ * can be performed to a hopper.
+ * 
+ * Strategies are chosen based on weights. Weights are manipulated based on
+ * success rates of each strategy.
+ * 
+ * performHillClimbing method performs hill climbing on a hopper, returns 
+ * an improved hopper.
+ *
+ */
+public class TribeBrain {
+	Genotype newGenotype; //genotype that leaves the brain
+
+	//map that stores the probability of each strategy occuring.
+	//strategy number - strategy weight
+	HashMap<Integer,Integer> strategyWeights = new HashMap<Integer,Integer>();
+
+	//number of strategies
+	int numberOfStrategies = 1;
+	//starting strategy weight
+	int startingWeight = 1;
+
+	//the index of the current strategy being performed
+	int currentStrat;
+
+	//object for the strategy that will be performed
+	Strategy strategy;
+
+	public TribeBrain(){
+		//initialize strategy map weights
+		initializeWeights();
+	}
+
+	
+	/**
+	 * This method takes in a hopper, performs hill climbing on it and
+	 * returns the updated hopper.
+	 * 
+	 *  
+	 * @param hopper - Hopper object that will get hill climbed
+	 * @return Hill climbed hopper
+	 */
+	public Hopper performHillClimbing(Hopper hopper){
+		//set the strategy to perform
+		strategy = newStrategy();
+		//clone hopper
+		Hopper clone = null;
+		try {
+			clone = new Hopper(hopper);
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (GeneticsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//perform the hill climbing on the clone
+		return strategy.climb(clone);
+	}
+
+	/*
+	 * Initialize the strategy weights in the map.
+	 */
+	private void initializeWeights() {
+		for(int i = 0; i < numberOfStrategies; i++){
+			strategyWeights.put(i, startingWeight);
+		}
+	}
+
+	/**
+	 * Pick a strategy based on probabilities/weights
+	 * 
+	 * @return i Strategy number.
+	 */
+	public int pickStrategy(){
+		int weightSum = 0;
+
+		//get sum of weights
+		for(int i = 0; i < strategyWeights.size(); i++){
+			weightSum += strategyWeights.get(i);
+		}
+
+		int choice = Helper.RANDOM.nextInt(weightSum);
+		int subTotal = 0;
+
+		//pick strategy based on weight
+		for(int i = 0; i < strategyWeights.size(); i++){
+			subTotal += strategyWeights.get(i);
+			if(choice < subTotal) return i;
+		}
+
+		return -1; //we shouldn't get here
+	}
+
+	public Strategy newStrategy(){
+		//pick the strategy number based on weights
+		int strat = pickStrategy();
+
+		//return a strategy object depending on the strategy number
+		//chosen above.
+		if(strat == 0){ //create new strategy 0
+			currentStrat = 0;
+			return new ChangeSingleAllele();
+		}
+		else if(strat == 1){ //strategy 1
+			currentStrat = 1;
+		} 
+		else if(strat == 2){ //strategy 2
+			currentStrat = 2;
+		}
+		else if(strat == 3){ //strategy 3
+			currentStrat = 3;
+		}
+		else if(strat == 4){ //strategy 4
+			currentStrat = 4;
+		}
+		else{ //something went wrong
+			currentStrat = -1;
+			return null;
+		}
+		return null;
+	}
+}
